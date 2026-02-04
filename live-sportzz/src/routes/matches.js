b51d104892f6efe7dce92223104decb20dbb4bbc
James-Kinuthia <kinuthiaa22019@gmail.com>
2026-02-04 21:56:30 +0300

    matches endpoint config
    configured the matches API to include the zod validations and proper structuring

---
+++ live-sportzz/src/routes/matches.js
@@ -0,0 +1,47 @@
+import { Router } from 'express'
+import { createMatchSchema, listMatchesQuerySchema } from '../validations/matches';
+import { matches } from '../db/schema';
+import { db } from '../db/db';
+import { parse } from 'dotenv';
+import { getMatchStatus } from '../utils/match-status';
+import { desc } from 'drizzle-orm';
+
+export const matchRouter = Router();
+const MAX_LIMIT = 100;
+
+matchRouter.get('/', async (req, res) => {
+    const parsed = listMatchesQuerySchema.safeParse(req.body);
+    if (!parsed.success) {
+        return res.status(400).json({ error: "Invalid query", details: JSON.stringify(parsed.error) })
+    }
+    const limit = Math.min(parsedData.data.limit ?? 50, MAX_LIMIT);
+    try {
+        const data = await db.select(matches).orderBy((desc(matches.createdAt))).limit(limit);
+        res.json({ data });
+    } catch (error) {
+        return res.status(500).json({ message: "Could not fetch queries", details: JSON.stringify(parsed.error) })
+    }
+});
+
+matchRouter.post('/', async (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body);
+    const { data: { startTime, endTime, homeScore, awayScore } } = parsed;
+
+    if (!parsed.success) {
+        return res.status(400).json({ error: 'Invalid payload.', details: JSON.stringify(parsed.error) });
+    }
+    try {
+        const [event] = await db.insert(matches).values({
+            ...parsed.data,
+            startTime: new Date(startTime),
+            endTime: new Date(endTime),
+            homeScore: homeScore ?? 0,
+            awayScore: awayScore ?? 0,
+            status: getMatchStatus(startTime, endTime),
+        }).returning();
+
+        res.status(201).json({ data: event });
+    } catch (error) {
+        res.status(500).json({ message: "Failed to create match.", details: JSON.stringify(error) });
+    }
+});
